76a40a07ccb95adc91905fd6a6becfd24218c928
Steve Persch <stevector@users.noreply.github.com>
2020-04-20 13:48:02 -0500

    lower mariahdb version

--- Install/Tasks.php
+++ Install/Tasks.php
@@ -7,6 +7,6 @@ use Drupal\Core\Database\Driver\mysql\Install\Tasks as CoreMysqlTasks;
 class Tasks extends CoreMysqlTasks {
 
   const MYSQL_MINIMUM_VERSION = '5.6';
-  const MARIADB_MINIMUM_VERSION = '10.1';
+  const MARIADB_MINIMUM_VERSION = '10.0';
 
 }
